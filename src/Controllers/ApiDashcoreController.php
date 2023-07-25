130057bc8604a7fbbd3f92cf81e2a9c3631dc2b1
Art <>
2023-07-25 14:57:53 +1200

    dashcore init components

---
+++ src/Controllers/ApiDashcoreController.php
@@ -0,0 +1,223 @@
+<?php
+
+namespace Goldfinch\Dashcore\Controllers;
+
+use SilverStripe\Security\Security;
+use SilverStripe\Control\Controller;
+use SilverStripe\Control\HTTPRequest;
+use Goldfinch\Dashcore\Services\PanelService;
+
+class ApiDashcoreController extends Controller
+{
+    private static $url_handlers = [
+        'POST dev/tasks' => 'devTasks',
+        'POST dev/build' => 'devBuild',
+        'POST info/table' => 'infoTable',
+        'POST search/page' => 'searchPage',
+        'POST info/robot' => 'infoRobot',
+        'POST info/sitemap' => 'infoSitemap',
+        'POST info/git' => 'infoGit',
+        'POST performance/info' => 'performanceInfo',
+        'POST performance/run' => 'performanceRun',
+        'POST bugtracker/info' => 'bugtrackerInfo',
+
+        'POST page/publish' => 'pagePublish',
+        'POST page/unpublish' => 'pageUnpublish',
+        'POST page/archive' => 'pageArchive',
+
+        'POST block/publish' => 'blockPublish',
+        'POST block/unpublish' => 'blockUnpublish',
+        'POST block/archive' => 'blockArchive',
+
+        'POST info/user' => 'infoUser',
+        'POST info/composer' => 'infoComposer',
+    ];
+
+    private static $allowed_actions = [
+        'devTasks',
+        'infoTable',
+        'searchPage',
+        'infoRobot',
+        'infoSitemap',
+        'infoGit',
+        'performanceInfo',
+        'performanceRun',
+        'bugtrackerInfo',
+        'pagePublish',
+        'pageUnpublish',
+        'pageArchive',
+        'blockPublish',
+        'blockUnpublish',
+        'blockArchive',
+
+        'infoComposer',
+        'infoUser',
+    ];
+
+    protected function init()
+    {
+        parent::init();
+
+        // ..
+    }
+
+    public function infoUser()
+    {
+        $user = Security::getCurrentUser();
+
+        $data = [
+            'email' => $user->Email,
+            'name' => $user->getName(),
+        ];
+
+        return json_encode($data);
+    }
+
+    public function devTasks()
+    {
+        //
+    }
+
+    public function infoComposer()
+    {
+        // 2) Composer installed packages
+        $installedPackages = PanelService::getComposerInstalledPackageList();
+
+        return json_encode($installedPackages);
+    }
+
+    public function infoTable()
+    {
+        // 1) Assets folder size
+        $assetsSize = PanelService::getAssetsSize();
+
+        // 3) Server data
+        $serverData = PanelService::getServerData();
+
+        // 4) SilverStripe version
+        $ss = PanelService::getSilverStripeVersion();
+
+        // 5) Mysql Size / amount of tables
+        // - TODO
+
+        // ---
+
+        $data = [
+          'assetsSize' => $assetsSize,
+
+          'server' => $serverData,
+
+          'ss' => $ss,
+        ];
+
+        return json_encode($data);
+    }
+
+    public function searchPage()
+    {
+        /**
+         * Search in sitemap
+         */
+    }
+
+    public function infoRobot()
+    {
+        /**
+         * - exists?
+         * - current page affected (blocked)?
+         * - source of file
+         */
+    }
+
+    public function infoSitemap()
+    {
+        /**
+         * - exists?
+         * - link to sitemap
+         * - current page included?
+         * - sitemap tree with all details, search, links
+         */
+    }
+
+    public function infoGit()
+    {
+        /**
+         * - last commit, ref, date, author
+         * - bitbucket pipelines status (last commit, date, author)
+         *
+         * https://developer.atlassian.com/cloud/bitbucket/rest/intro/#authentication_old
+         */
+
+        // curl -X GET -u bitbucket_username:app_password "https://api.bitbucket.org/2.0/repositories/<workspace>/<repository>/pipelines/?status=PENDING&status=BUILDING&status=IN_PROGRESS"
+        // PENDING,BUILDING,IN_PROGRESS
+
+        return json_encode([
+          'commits' => PanelService::getGitCommits(10),
+          'branches' => PanelService::getGitBranches(),
+          'mainbranch' => PanelService::getGitCurrentBranch(),
+        ]);
+    }
+
+    public function performanceInfo()
+    {
+        /**
+         * -
+         */
+    }
+
+    public function performanceRun()
+    {
+        /**
+         * -
+         */
+    }
+
+    public function bugtrackerInfo()
+    {
+        /**
+         * -
+         */
+    }
+
+    public function pagePublish()
+    {
+        /**
+         * -
+         */
+    }
+
+    public function pageUnpublish()
+    {
+        /**
+         * -
+         */
+    }
+
+    public function pageArchive()
+    {
+        /**
+         * -
+         */
+    }
+
+    public function blockPublish()
+    {
+        /**
+         * -
+         */
+    }
+
+    public function blockUnpublish()
+    {
+        /**
+         * -
+         */
+    }
+
+    public function blockArchive()
+    {
+        /**
+         * -
+         */
+    }
+}
---
+++ src/Services/DashService.php
@@ -0,0 +1,270 @@
+<?php
+
+namespace Goldfinch\Dashcore\Services;
+
+use SilverStripe\Core\Environment;
+use SilverStripe\Control\Controller;
+use SilverStripe\Security\Security;
+use Composer\InstalledVersions;
+use SilverStripe\Control\Director;
+use SilverStripe\Core\Manifest\VersionProvider;
+use SilverStripe\Security\InheritedPermissions;
+use SilverStripe\Versioned\Versioned;
+use SilverStripe\SiteConfig\SiteConfig;
+
+class DashService
+{
+    /**
+     * Gets the size of public/assets folder
+     */
+
+    public static function getAssetsSize(): string
+    {
+        $f = BASE_PATH . '/public/assets/';
+        // $io = popen('/usr/bin/du -sh' . $f, 'r');
+        // $assetsSize = fgets($io, 4096);
+        // $assetsSize = substr($assetsSize, 0, strpos($assetsSize, '\t'));
+        // pclose($io);
+
+        $assetsSize = 0;
+        foreach (new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($f)) as $file) {
+            $assetsSize += $file->getSize();
+        }
+
+        return round($assetsSize / 1000000, 2);
+    }
+
+    /**
+     * Gets list of installed composer packages
+     */
+    public static function getComposerInstalledPackageList(): array
+    {
+        $list = [];
+
+        foreach(InstalledVersions::getInstalledPackages() as $package)
+        {
+            $list[] = [
+              'name' => $package,
+              'version' => InstalledVersions::getVersion($package)
+            ];
+        }
+
+        return $list;
+    }
+
+    /**
+     * Gets some useful server data
+     */
+    public static function getServerData(): array
+    {
+        $data = [
+            'dns' => dns_get_record(Director::host()),
+
+            'disk_total' => round((disk_total_space('/') / 1000000) / 1000, 2), // gb
+            'timezone' => date_default_timezone_get(),
+            'server_ip' => $_SERVER['SERVER_ADDR'],
+            'server_email' => $_SERVER['SERVER_ADMIN'],
+            'cdn' => isset($_SERVER['HTTP_CDN_LOOP']) ? $_SERVER['HTTP_CDN_LOOP'] : null, // cloudflare
+            'server_ipcountry' => isset($_SERVER['HTTP_CF_IPCOUNTRY']) ? $_SERVER['HTTP_CF_IPCOUNTRY'] : null,
+            'php' => phpversion(),
+            'allocated_php_memory' => round(memory_get_usage() / 1000000, 2), // mb
+            'mysql' => mysqli_get_client_info(), // if used?
+        ];
+
+        return $data;
+    }
+
+    /**
+     * Gets SilverStripe version
+     */
+    public static function getSilverStripeVersion(): array
+    {
+        $versionProvider = new VersionProvider;
+
+        return [
+            'version' => $versionProvider->getVersion(),
+            'modules' => $versionProvider->getModules(),
+        ];
+    }
+
+    /**
+     * Gets latest git commits
+     */
+    public static function getGitCommits($limit = 10): array | null
+    {
+        $path = BASE_PATH . '/.git/';
+
+        if (file_exists($path))
+        {
+            chdir(BASE_PATH);
+            exec('git log -' . $limit . ' --pretty=\'%H|-|-|%s|-|-|%an|-|-|%ae|-|-|%aD\'', $logs);
+
+            $list = [];
+
+            foreach($logs as $log)
+            {
+                $commit = explode('|-|-|', $log);
+
+                $list[] = [
+                  'hash' => $commit[0],
+                  'commit' => $commit[1],
+                  'author' => $commit[2],
+                  'email' => $commit[3],
+                  'date' => $commit[4],
+                ];
+            }
+
+            return $list;
+        }
+
+        return null;
+    }
+
+    /**
+     * Gets all branches
+     */
+    public static function getGitBranches(): array | null
+    {
+        $path = BASE_PATH . '/.git/';
+
+        if (file_exists($path))
+        {
+            chdir(BASE_PATH);
+            exec('git branch', $branchs);
+
+            $list = [];
+
+              foreach($branchs as $key => $b)
+              {
+                  $list[] = [
+                    'main' => $b[0] == '*',
+                    'name' => last(explode(' ', $b)),
+                  ];
+              }
+
+            return $list;
+        }
+
+        return null;
+    }
+
+    /**
+     * Gets main branch
+     */
+    public static function getGitCurrentBranch(): string | null
+    {
+        $branches = self::getGitBranches();
+
+        if ($branches)
+        {
+            $return = null;
+
+            foreach($branches as $branch)
+            {
+                if ($branch['main'])
+                {
+                    $return = $branch['name'];
+                    break;
+                }
+            }
+
+            return $return;
+        }
+
+        return null;
+    }
+
+    /**
+     * Gets all kinds of data for the initial dashpanel setup
+     */
+    public static function getPanelInitialData()
+    {
+        if (Controller::has_curr())
+        {
+            $object = Controller::curr();
+
+            $cfg = SiteConfig::current_site_config();
+
+            $user = Security::getCurrentUser();
+
+            // Session info
+            // $member->LoginSessions()->first();
+            // $currentSessions = $member->LoginSessions()->filterAny([
+            //     'Persistent' => 1,
+            //     'LastAccessed:GreaterThan' => date('Y-m-d H:i:s', $maxAge)
+            // ]);
+            // getSchemaDataDefaults
+
+            // MFA
+            // $user->getDefaultRegisteredMethodName();
+            // $user->getDefaultRegisteredMethod();
+
+            // check for inherit
+            if ($object->CanViewType === InheritedPermissions::INHERIT)
+            {
+                if ($object->ParentID)
+                {
+                    $CanViewType = $object->Parent()->CanViewType;
+                }
+                else
+                {
+                    $CanViewType = $object->getSiteConfig()->CanViewType;
+                }
+            }
+            else
+            {
+                $CanViewType = $object->CanViewType;
+            }
+
+            $firstVersion = Versioned::get_version($object->ClassName, $object->ID, 1);
+            $lastVersion = Versioned::get_latest_version($object->ClassName, $object->ID);
+
+            return [
+              'env' => Environment::getEnv('SS_ENVIRONMENT_TYPE'),
+              'siteAccess' => $cfg->CanViewType,
+
+              'page' => [
+                'versions' => [
+                  'first' => [
+                    'version_id' => $firstVersion->Version,
+                    'updatedAt' => $firstVersion->LastEdited,
+                    'author' => $firstVersion->Author() ? [
+                      'name' => $firstVersion->Author()->getName(),
+                      'link' => '/admin/security/users/EditForm/field/users/item/'.$firstVersion->Author()->ID.'/edit'
+                    ] : null,
+                  ],
+                  'last' => [
+                    'version_id' => $lastVersion->Version,
+                    'updatedAt' => $lastVersion->LastEdited,
+                    'author' => $lastVersion->Author() ? [
+                      'name' => $lastVersion->Author()->getName(),
+                      'link' => '/admin/security/users/EditForm/field/users/item/'.$lastVersion->Author()->ID.'/edit'
+                    ] : null,
+                  ],
+                ],
+                'icon' => substr($object->getIconClass(), 0, 3) === 'bi-' ? substr($object->getIconClass(), 3) : $object->getIconClass(),
+                'classNamespace' => $object->ClassName,
+                'className' => last(explode('\\', $object->ClassName)),
+                'canViewType' => $CanViewType,
+                'createdAt' => $object->Created,
+                'updatedAt' => $object->LastEdited,
+                'isOnDraft' => $object->isOnDraft(),
+                'isPublished' => $object->isPublished(),
+                'stagesDiffer' => $object->stagesDiffer(),
+                'canPublish' => $object->canPublish(),
+                'canUnpublish' => $object->canUnpublish(),
+                'canEdit' => $object->canEdit(),
+              ],
+
+              'user' => [
+                'email' => $user->email,
+                'firstname' => $user->FirstName,
+                'surname' => $user->Surname,
+                'groups' => $user->Groups()->map('ID')->toArray(),
+              ],
+            ];
+        }
+
+        return null;
+    }
+}
