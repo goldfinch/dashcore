e0de3a080a07b9d8b994d2b0dd1abfbe6454389f
Art Karasev <as.karasyov@gmail.com>
2023-10-31 11:26:47 +1300

    new cards

--- src/Controllers/ApiDashcoreController.php
+++ src/Controllers/ApiDashcoreController.php
@@ -3,12 +3,23 @@
 namespace Goldfinch\Dashcore\Controllers;
 
 use Carbon\Carbon;
+use ReflectionMethod;
+use SilverStripe\ORM\DB;
+use SilverStripe\Assets\File;
+use SilverStripe\Dev\Deprecation;
 use SilverStripe\Control\Director;
+use SilverStripe\Core\Environment;
+use SilverStripe\Admin\LeftAndMain;
 use SilverStripe\Security\Security;
 use SilverStripe\CMS\Model\SiteTree;
 use SilverStripe\Control\Controller;
+use SilverStripe\Core\Config\Config;
 use SilverStripe\Control\HTTPRequest;
+use DNADesign\Elemental\Models\BaseElement;
+use SilverStripe\ORM\Connect\MySQLDatabase;
 use Goldfinch\Dashcore\Services\DashService;
+use SilverStripe\AssetAdmin\Forms\FileFormFactory;
+use SilverStripe\AssetAdmin\Forms\ImageFormFactory;
 
 class ApiDashcoreController extends Controller
 {
@@ -36,6 +47,9 @@ class ApiDashcoreController extends Controller
         'POST info/composer' => 'infoComposer',
 
         'POST info/sitetree' => 'infoSitetree',
+        'POST info/elementalarea' => 'infoElementalarea',
+        'POST info/siteassets' => 'infoSitassets',
+        'POST info/server' => 'infoServer',
     ];
 
     private static $allowed_actions = [
@@ -59,6 +73,9 @@ class ApiDashcoreController extends Controller
         'infoUser',
 
         'infoSitetree',
+        'infoElementalarea',
+        'infoSitassets',
+        'infoServer',
     ];
 
     protected function init()
@@ -66,6 +83,188 @@ class ApiDashcoreController extends Controller
         parent::init();
 
         // ..
+    }
+
+    public function infoServer()
+    {
+        $list = [];
+
+        $list[] = [
+          'label' => 'PHP',
+          'value' => phpversion(),
+        ];
+
+        $list[] = [
+          'label' => 'MySQL',
+          'value' => DB::get_conn()->getVersion(),
+        ];
+
+        $list[] = [
+          'label' => 'Timezone',
+          'value' => date_default_timezone_get(),
+        ];
+
+        $list[] = [
+          'label' => 'SS',
+          'value' => LeftAndMain::singleton()->CMSVersionNumber(),
+        ];
+
+        $list[] = [
+          'label' => 'SS environment',
+          'value' => Environment::getEnv('SS_ENVIRONMENT_TYPE'),
+        ];
+
+        $list[] = [
+          'label' => 'Server IP',
+          'value' => $_SERVER['SERVER_ADDR'],
+        ];
+
+        $list[] = [
+          'label' => 'Main domain',
+          'value' => $_SERVER['SERVER_NAME'],
+        ];
+
+        $dns = dns_get_record($_SERVER['SERVER_NAME']);
+
+        if (!empty($dns))
+        {
+            $dnsString = '';
+
+            sort($dns);
+
+            foreach ($dns as $d)
+            {
+                if ($d['type'] == 'NS')
+                {
+                    $dnsString .= $d['target'] . ', ';
+                }
+            }
+
+            $dnsString = substr($dnsString, 0, -2);
+
+            $list[] = [
+              'label' => 'DNS Server',
+              'value' => $dnsString,
+            ];
+        }
+
+        $list[] = [
+          'label' => 'Memory limit',
+          'value' => ini_get('memory_limit'),
+        ];
+
+        $list[] = [
+          'label' => 'Upload max filesize',
+          'value' => ini_get('upload_max_filesize'),
+        ];
+
+        $list[] = [
+          'label' => 'Max execution time',
+          'value' => ini_get('max_execution_time'),
+        ];
+
+        $list[] = [
+          'label' => 'Post max size',
+          'value' => ini_get('post_max_size'),
+        ];
+
+        if (ini_get('display_errors'))
+        {
+            $list[] = [
+              'label' => 'Display errors',
+              'value' => ini_get('display_errors'),
+            ];
+        }
+
+        if (ini_get('expose_php'))
+        {
+            $list[] = [
+              'label' => 'Expose php',
+              'value' => ini_get('expose_php'),
+            ];
+        }
+
+        $data = [
+          'list' => $list,
+          'add_link' => '',
+        ];
+
+        return json_encode($data);
+    }
+
+    public function infoSitassets()
+    {
+        $list = [];
+
+        foreach(File::get()->sort('LastEdited', 'DESC')->limit(5) as $item)
+        {
+            if (!$item || !$item->exists()) {
+                return null;
+            }
+
+            $lastversion = $item->get_latest_version($item->ClassName, $item->ID);
+            // vendor/silverstripe/asset-admin/code/Controller/AssetAdmin.php
+
+            if (method_exists($item, 'FitMax'))
+            {
+                $icon = $item->FitMax(352, 264)->getURL();
+
+                $r = new ReflectionMethod(ImageFormFactory::class, 'getSpecsMarkup');
+                $r->setAccessible(true);
+                $fileSpecs = $r->invoke(new ImageFormFactory(), $item);
+            }
+            else
+            {
+                $icon = $item->getIcon();
+
+                $r = new ReflectionMethod(ImageFormFactory::class, 'getSpecsMarkup');
+                $r->setAccessible(true);
+                $fileSpecs = $r->invoke(new FileFormFactory(), $item);
+            }
+
+            $list[] = [
+              'icon' => $icon,
+              'specs' => $fileSpecs,
+              'title' => $item->Title,
+              'link' => $item->CMSEditLink(),
+              'author' => $lastversion->Author() ? $lastversion->Author()->getName() : null,
+              'updated_at' => Carbon::parse($item->LastEdited)->format('l, F jS Y, H:i'),
+              'updated_at_human' => Carbon::parse($item->LastEdited)->diffForHumans(),
+            ];
+        }
+
+        $data = [
+          'list' => $list,
+          'add_link' => '',
+        ];
+
+        return json_encode($data);
+    }
+
+    public function infoElementalarea()
+    {
+        $list = [];
+
+        foreach(BaseElement::get()->sort('LastEdited', 'DESC')->limit(5) as $item)
+        {
+            $lastversion = $item->get_latest_version($item->ClassName, $item->ID);
+
+            $list[] = [
+              'icon' => trim($item->getIcon()->RAW()),
+              'title' => $item->Title,
+              'link' => $item->CMSEditLink(),
+              'author' => $lastversion->Author() ? $lastversion->Author()->getName() : null,
+              'updated_at' => Carbon::parse($item->LastEdited)->format('l, F jS Y, H:i'),
+              'updated_at_human' => Carbon::parse($item->LastEdited)->diffForHumans(),
+            ];
+        }
+
+        $data = [
+          'list' => $list,
+          'add_link' => '',
+        ];
+
+        return json_encode($data);
     }
 
     public function infoSitetree()
